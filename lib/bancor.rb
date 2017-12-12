faa0e4db68ed213d9cf99ff0460f31c090a35b21
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-12-12 17:40:13 +0900

    module Bancor

--- lib/bancor.rb
+++ lib/bancor.rb
@@ -1,5 +1,29 @@
 require "bancor/version"
 
 module Bancor
-  # Your code goes here...
+  attr_accessor :total_supply, :price
+
+  ETH = 300000.freeze        # 300000(ETH)
+  CRR = 0.2.freeze           # CRR = 20%
+  RATE = 1.freeze            # Rate 1ETH = 1BNT
+
+  def initialize
+    @total_supply = ETH
+    @reserved_token = ETH * 0.2 # ETH 20% to reserve. CRR = 20%
+    @price = @reserved_token / (@total_supply * CRR)
+  end
+
+  def buying(quantity)
+    token = @total_supply * (((1 + (quantity / @reserved_token)) ** CRR) - 1)
+    @reserved_token = @reserved_token + quantity
+    @total_supply = @total_supply + token
+    @price = @reserved_token / (@total_supply * CRR)
+  end
+
+  def selling(quantity)
+    token = @reserved_token * (1 - ((1 - (quantity / @total_supply)) ** (1/CRR)))
+    @reserved_token = @reserved_token - token
+    @total_supply = @total_supply - quantity
+    @price = @reserved_token / (@total_supply * CRR)
+  end
 end
