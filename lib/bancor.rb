4a6faeaba8545820bf8541a7c698e75fef8f342f
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-12-14 22:36:17 +0900

    update methods and variables.

--- lib/bancor.rb
+++ lib/bancor.rb
@@ -2,31 +2,27 @@ require "bancor/version"
 
 module Bancor
   class Protocol
-
-    attr_accessor :total_supply, :price
-
-    ETH = 300000.freeze        # 300000(ETH)
-    CRR = 0.2.freeze           # CRR = 20%
-    RATE = 1.freeze            # Rate 1ETH = 1BNT
+    attr_reader :total_supply, :reserved_token, :price
 
     def initialize(eth:, rate:, crr:)
       @total_supply = eth * rate
-      @reserved_token = @total_supply * crr # ETH 20% to reserve. CRR = 20%
+      @reserved_token = @total_supply * crr
       @price = @reserved_token / (@total_supply * crr)
+      @crr = crr
     end
 
     def buy(quantity)
-      token = @total_supply * (((1 + (quantity / @reserved_token)) ** CRR) - 1)
+      token = @total_supply * (((1 + (quantity / @reserved_token)) ** @crr) - 1)
       @reserved_token = @reserved_token + quantity
       @total_supply = @total_supply + token
-      @price = @reserved_token / (@total_supply * CRR)
+      @price = @reserved_token / (@total_supply * @crr)
     end
 
     def sell(quantity)
-      token = @reserved_token * (1 - ((1 - (quantity / @total_supply)) ** (1/CRR)))
+      token = @reserved_token * (1 - ((1 - (quantity / @total_supply)) ** (1/@crr)))
       @reserved_token = @reserved_token - token
       @total_supply = @total_supply - quantity
-      @price = @reserved_token / (@total_supply * CRR)
+      @price = @reserved_token / (@total_supply * @crr)
     end
   end
 end
