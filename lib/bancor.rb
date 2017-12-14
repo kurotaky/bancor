c618067f930ddf33d900e58026c15b58a317479f
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-12-14 21:34:36 +0900

    class Bancor::Protocol

--- lib/bancor.rb
+++ lib/bancor.rb
@@ -1,29 +1,32 @@
 require "bancor/version"
 
 module Bancor
-  attr_accessor :total_supply, :price
+  class Protocol
 
-  ETH = 300000.freeze        # 300000(ETH)
-  CRR = 0.2.freeze           # CRR = 20%
-  RATE = 1.freeze            # Rate 1ETH = 1BNT
+    attr_accessor :total_supply, :price
 
-  def initialize
-    @total_supply = ETH
-    @reserved_token = ETH * 0.2 # ETH 20% to reserve. CRR = 20%
-    @price = @reserved_token / (@total_supply * CRR)
-  end
+    ETH = 300000.freeze        # 300000(ETH)
+    CRR = 0.2.freeze           # CRR = 20%
+    RATE = 1.freeze            # Rate 1ETH = 1BNT
 
-  def buying(quantity)
-    token = @total_supply * (((1 + (quantity / @reserved_token)) ** CRR) - 1)
-    @reserved_token = @reserved_token + quantity
-    @total_supply = @total_supply + token
-    @price = @reserved_token / (@total_supply * CRR)
-  end
+    def initialize(eth:, rate:, crr:)
+      @total_supply = eth * rate
+      @reserved_token = @total_supply * crr # ETH 20% to reserve. CRR = 20%
+      @price = @reserved_token / (@total_supply * crr)
+    end
 
-  def selling(quantity)
-    token = @reserved_token * (1 - ((1 - (quantity / @total_supply)) ** (1/CRR)))
-    @reserved_token = @reserved_token - token
-    @total_supply = @total_supply - quantity
-    @price = @reserved_token / (@total_supply * CRR)
+    def buy(quantity)
+      token = @total_supply * (((1 + (quantity / @reserved_token)) ** CRR) - 1)
+      @reserved_token = @reserved_token + quantity
+      @total_supply = @total_supply + token
+      @price = @reserved_token / (@total_supply * CRR)
+    end
+
+    def sell(quantity)
+      token = @reserved_token * (1 - ((1 - (quantity / @total_supply)) ** (1/CRR)))
+      @reserved_token = @reserved_token - token
+      @total_supply = @total_supply - quantity
+      @price = @reserved_token / (@total_supply * CRR)
+    end
   end
 end
