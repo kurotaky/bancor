d9e4f72a6e2e86ada5eee016a4f636c58c3e8e0c
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-12-19 15:30:12 +0900

    add pricing methods

--- lib/bancor.rb
+++ lib/bancor.rb
@@ -2,7 +2,7 @@ require "bancor/version"
 
 module Bancor
   class Protocol
-    attr_reader :total_supply, :reserved_token, :price
+    attr_reader :total_supply, :reserved_token, :price, :transaction_price
 
     def initialize(eth:, rate:, crr:)
       @total_supply = eth * rate
@@ -22,6 +22,20 @@ module Bancor
       token = @reserved_token * (1 - ((1 - (quantity / @total_supply)) ** (1/@crr)))
       @reserved_token = @reserved_token - token
       @total_supply = @total_supply - quantity
+      @price = @reserved_token / (@total_supply * @crr)
+    end
+
+    def pricing_to_buy(quantity)
+      @transaction_price = @reserved_token * ((((quantity / @total_supply) + 1) ** (1/@crr)) - 1)
+      @total_supply = @total_supply + quantity
+      @reserved_token = @reserved_token + @transaction_price
+      @price = @reserved_token / (@total_supply * @crr)
+    end
+
+    def pricing_to_sell(quantity)
+      @transaction_price = @reserved_token * (1 - ((1 - (quantity / @total_supply)) ** (1/@crr)))
+      @total_supply = @total_supply - quantity
+      @reserved_token = @reserved_token - @transaction_price
       @price = @reserved_token / (@total_supply * @crr)
     end
   end
